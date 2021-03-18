afde201787e4116899b7e1b56cce0207954955b6
cybersi-io <66743889+cybersi-io@users.noreply.github.com>
2021-03-18 12:45:34 +0000

    Added basic Tkinter GUI window

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 # Imports
-
+from tkinter import *
 from PIL import Image 
 
-#open image in png format 
-img = Image.open(r'C:\Users\user1\Desktop/image1.png') 
+# #open image in png format 
+# img = Image.open(r'C:\Users\user1\Desktop/image1.png') 
 
-img = img.convert('RGB')
+# img = img.convert('RGB')
 
-#The image object is used to save the image in jpg format 
+# #The image object is used to save the image in jpg format 
 
-img.save(r'C:\Users\user1\Desktop/new_image.jpg')
+# img.save(r'C:\Users\user1\Desktop/new_image.jpg')
+
+root = Tk()
+root.title("Image Converter")
+
+
+root.mainloop()
